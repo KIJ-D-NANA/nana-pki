fd82f051747ebe3f52703fed3eafaee709e41b1d
natsumiaya <natsumiaya@live.com>
2015-05-06 20:05:51 +0000

    filled admin header

--- application/views/page-user-csr.php
+++ application/views/page-user-csr.php
@@ -1,44 +1,67 @@
-    <script>
-        $(document).ready(function () {
-            $("input#submit").click(function(){
-                $.ajax({
-                    type: "POST",
-                    url: "process.php", // 
-                    data: $('form.contact').serialize(),
-                    success: function(msg){
-                        $("#thanks").html(msg)
-                        $("#form-content").modal('hide');   
-                    },
-                    error: function(){
-                        alert("failure");
-                    }
-                });
-            });
-        });
-    </script>
-    
 
     <!--Page Content-->
         <div class="section">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
-                        <table class="table-striped table-condensed text-center" style="margin:auto;width:80%;"  >
+                        <table class="table-striped table-condensed text-center" style="margin:auto;width:100%;"  >
                         <tr class="text-center">
 	    			            <th class="text-center">CSR ID</th>
+	    			            <th class="text-center">Common Name</th>
+	    			            <th class="text-center">Organization Unit</th>
+	    			            <th class="text-center">Organization</th>
+	    			            <th class="text-center">City</th>
+	    			            <th class="text-center">State</th>
+	    			            <th class="text-center">Country</th>
 	    			            <th class="text-center">Status</th>
 	    			    </tr>
-	    			    <?php
-                        $i = 0;
-                        foreach($pack as $row)
-                        {
-                            echo '<tr>';
-                            echo '<td> '.$pack[$i]["csr_id"].'</td>';
-                            echo '<td>Pending</td>';
-                            echo '</tr>';
-                            
-                        }   
-                        ?>
+	    			    <?php 
+    	    			    $i = 0;
+    	    				foreach($pack as $row)
+    	    				{
+    	    					echo '<tr>';
+    	    					echo '<td>'.$pack[$i]["csr_id"].'</td>';
+    	    						if(isset($pack[$i]["dn"]["CN"])){
+    	    							echo '<td>'.$pack[$i]["dn"]["CN"].'</td>';
+    	    						}
+    	    						else {
+    	    							echo '<td>-</td>';
+    	    						}
+    	    						if(isset($pack[$i]["dn"]["OU"])){
+    	    							echo '<td>'.$pack[$i]["dn"]["OU"].'</td>';
+    	    						}
+    	    						else {
+    	    							echo '<td>-</td>';
+    	    						}
+    	    						if(isset($pack[$i]["dn"]["O"])){
+    	    							echo '<td>'.$pack[$i]["dn"]["O"].'</td>';
+    	    						}
+    	    						else {
+    	    							echo '<td>-</td>';
+    	    						}
+    	    						if(isset($pack[$i]["dn"]["L"])){
+    	    							echo '<td>'.$pack[$i]["dn"]["L"].'</td>';
+    	    						}
+    	    						else {
+    	    							echo '<td>-</td>';
+    	    						}
+    	    						if(isset($pack[$i]["dn"]["ST"])){
+    	    							echo '<td>'.$pack[$i]["dn"]["ST"].'</td>';
+    	    						}
+    	    						else {
+    	    							echo '<td>-</td>';
+    	    						}
+    	    						if(isset($pack[$i]["dn"]["C"])){
+    	    							echo '<td>'.$pack[$i]["dn"]["C"].'</td>';
+    	    						}
+    	    						else {
+    	    							echo '<td>-</td>';
+    	    						}
+    	    					echo '<td>Pending</td>';  
+    	    					echo '</tr>';
+    	    				}	
+    	    				
+	    			    ?>
                         </table>
                     </div>
                 </div>
