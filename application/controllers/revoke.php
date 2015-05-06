00fdb81ba2d42a93b5ff3d192e61948983b65c4a
Reyhan Arief <reisuke.raizan@gmail.com>
2015-05-06 00:01:19 +0000

    add revoke page. controller. get data

--- application/controllers/revoke.php
+++ application/controllers/revoke.php
@@ -4,22 +4,49 @@ class Crl extends CI_Controller {
 
     public function __construct(){
         parent::__construct();
+        include(APPPATH.'library/phpseclib/Crypt/RSA.php');
+        include(APPPATH.'library/phpseclib/File/X509.php');
         if ($this->session->userdata['user_id'] == "admin"){
             redirect(site_url('login'));
         }
+        $this->load->model('certs_model');
+    }
+    
+    public function index(){
+        $data["cert"] = $this->certs_model->getCertificateList();
+        $this->load->view('page-torevoke', $data);
     }
     
     public function generate_crl(){
-        $cert = "file://path/to/ca.cert.pem";
-        $key = "file://path/to/ca.key.pem";
-        $crl = "file://path/to/crl.pem";
-        $generating = proc_open("openssl ca -keyfile $key -cert $cert -gencrl -out $crl");
+        // Load the CA and its private key.
+        $pemcakey = file_get_contents('myCAprivkey.pem');
+        $cakey = new Crypt_RSA();
+        $cakey->loadKey($pemcakey);
+        $pemca = file_get_contents('myCA.pem');
+        $ca = new File_X509();
+        $ca->loadX509($pemca);
+        $ca->setPrivateKey($cakey);
+        
+        // Load the CRL.
+        $crl = new File_X509();
+        $crl->loadCA($pemca); // For later signature check.
+        $pemcrl = file_get_contents('myCRL.pem');
+        $crl->loadCRL($pemcrl);
+        
+        // Validate the CRL.
+        if ($crl->validateSignature() !== 1) {
+            exit("CRL signature is invalid\n");
+        }
+        
+        // Update the revocation list.
+        $crl->setRevokedCertificateExtension('4321', 'id-ce-cRLReasons', 'privilegeWithdrawn');
+        
+        // Generate the new CRL.
+        $crl->setEndDate('+3 months');
+        $newcrl = $crl->signCRL($ca, $crl);
+        
+        // Output it.
+        echo $crl->saveCRL($newcrl) . "\n";
     }
     
-    public function revoke(){
-        $privatekey = "ca.key.pem";
-        $cert = "ca.cert.pem";
-        $usercert = "someone.cert.pem";
-        $revoking = proc_open("openssl ca -keyfile $privatekey -cert $cert -revoke $usercert");
-    }
 }
--- application/models/certs_model.php
+++ application/models/certs_model.php
@@ -5,52 +5,56 @@ class Certs_model extends CI_Model {
     public function __construct() {
         parent::__construct();
     }
-    public function createCert(){
-        // Fill in data for the distinguished name to be used in the cert
-        // You must change the values of these keys to match your name and
-        // company, or more precisely, the name and company of the person/site
-        // that you are generating the certificate for.
-        // For SSL certificates, the commonName is usually the domain name of
-        // that will be using the certificate, but for S/MIME certificates,
-        // the commonName will be the name of the individual who will use the
-        // certificate.
-        $dn = array(
-            "countryName" => "UK",
-            "stateOrProvinceName" => "Somerset",
-            "localityName" => "Glastonbury",
-            "organizationName" => "The Brain Room Limited",
-            "organizationalUnitName" => "PHP Documentation Team",
-            "commonName" => "Wez Furlong",
-            "emailAddress" => "wez@example.com"
-        );
-        
-        // Generate a new private (and public) key pair
-        $privkey = openssl_pkey_new();
-        
-        // Generate a certificate signing request
-        $csr = openssl_csr_new($dn, $privkey);
-        
-        // You will usually want to create a self-signed certificate at this
-        // point until your CA fulfills your request.
-        // This creates a self-signed cert that is valid for 365 days
-        $sscert = openssl_csr_sign($csr, null, $privkey, 365);
-        
-        // Now you will want to preserve your private key, CSR and self-signed
-        // cert so that they can be installed into your web server, mail server
-        // or mail client (depending on the intended use of the certificate).
-        // This example shows how to get those things into variables, but you
-        // can also store them directly into files.
-        // Typically, you will send the CSR on to your CA who will then issue
-        // you with the "real" certificate.
-        openssl_csr_export($csr, $csrout) and var_dump($csrout);
-        openssl_x509_export($sscert, $certout) and var_dump($certout);
-        openssl_pkey_export($privkey, $pkeyout, "mypassword") and var_dump($pkeyout);
-        
-        // Show any errors that occurred here
-        while (($e = openssl_error_string()) !== false) {
-            echo $e . "\n";
+    
+    public function getCertificateList(){
+        $sql = 'select * from certificates';
+        $query = $this->db->query($sql);
+        $i = 0;
+        foreach ($query->result() as row){
+            $cert[$i]["serial_number"] = $row->serial_number;
+            $i = $i + 1;
         }
-        
+        return $cert;
     }
+    
 
+    public function checkSerial($serial) {
+        $sql = 'select * from certificates where serial_number = \''.$serial.'\'';
+        $query = $this->db->query($sql);
+        if ($query->num_rows() > 0) {
+           return true; // Found
+        }
+        else {
+            return false; // Not found
+        }
+    }
+    
+    public function saveCertificate($serial, $csr_id, $cert) {
+        $user_id = $this->session->userdata('user_id');
+        $sql = 'insert into certificates values(\''.$serial.'\',\''.$user_id.'\''.',\''.$csr_id.'\''.',\''.$cert.'\')';
+        $this->db->query($sql);
+    }
+    
+    private function generate_uuid_v4() {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+
+            // 32 bits for "time_low"
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+
+            // 16 bits for "time_mid"
+            mt_rand(0, 0xffff),
+
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand(0, 0x0fff) | 0x4000,
+
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand(0, 0x3fff) | 0x8000,
+
+            // 48 bits for "node"
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
 }
